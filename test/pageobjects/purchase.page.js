a11cd58a5a9ed56a0ae9ec97b2672a5b5ea43c13
matiasraviola <matiasaraviola@gmail.com>
2022-11-28 17:31:23 -0300

    TG-15: test with/without checkout information

--- test/pageobjects/purchase.page.js
+++ test/pageobjects/purchase.page.js
@@ -33,8 +33,41 @@ class PurchasePage {
     get btnCart () {
         return $('#shopping_cart_container');
     }
-    get btnAddCart () {
+    get btnAddBikeLight () {
         return $('#add-to-cart-sauce-labs-bike-light');
+    }
+    get btnAddBackpack () {
+        return $('#add-to-cart-sauce-labs-backpack');
+    }
+    get btnAddBoltTshirt () {
+        return $('#add-to-cart-sauce-labs-bolt-t-shirt');
+    }
+    get btnAddFleeceJacket () {
+        return $('#add-to-cart-sauce-labs-fleece-jacket');
+    }
+    get btnAddTshirtRed () {
+        return $('//*[@id="add-to-cart-test.allthethings()-t-shirt-(red)"]');
+    }
+    get btnAddLabsOnesie () {
+        return $('#add-to-cart-sauce-labs-onesie');
+    }
+    get btnRemoveBackpack () {
+        return $('#remove-sauce-labs-backpack');
+    }
+    get btnRemoveBikeLight () {
+        return $('#remove-sauce-labs-bike-light');
+    }
+    get btnRemoveBoltTshiert () {
+        return $('#remove-sauce-labs-bolt-t-shirt');
+    }
+    get btnRemoveFleeceJacket () {
+        return $('#remove-sauce-labs-fleece-jacket');
+    }
+    get btnRemoveLabsOnesie () {
+        return $('#remove-sauce-labs-onesie');
+    }
+    get btnRemoveTshirtRed () {
+        return $('#//*[@id="remove-test.allthethings()-t-shirt-(red)"]');
     }
     get btnCheckout () {
         return $('#checkout');
@@ -42,9 +75,16 @@ class PurchasePage {
     get hamburMenu () {
         return $('#react-burger-menu-btn');
     }
+    get resetCart () {
+        return $('#reset_sidebar_link');
+    }
+    get msgError () {
+        return $('#error-message-container error');
+    }
     get logout () {
         return $('#logout_sidebar_link');
     }
+
     /**
      * a method to encapsule automation code to interact with the page
      * e.g. to login using username and password
--- test/specs/purchase.e2e.js
+++ test/specs/purchase.e2e.js
@@ -1,19 +1,90 @@
-import purchasePage from '../pageobjects/purchase.page';
 import PurchasePage from  '../pageobjects/purchase.page';
 
 describe('My Login application', () => {
     beforeAll ('Navigate to url',() => {
         browser.url("https://www.saucedemo.com/");
     })
-    it('should login with valid credentials', async () => {
+    it('add some product to the cart and complete the sell', async () => {
         await PurchasePage.login('standard_user','secret_sauce');
         await expect(browser).toHaveUrl("https://www.saucedemo.com/inventory.html");
-        await browser.pause(3000);
-        await PurchasePage.btnAddCart.click();
+        await PurchasePage.btnAddBackpack.waitForClickable();
+        await PurchasePage.btnAddBackpack.click();
+        const cartNumber = await $('#shopping_cart_container > a > span').getText();
+        await expect(cartNumber).toBe('1')
+        await PurchasePage.btnCart.waitForClickable();
         await PurchasePage.btnCart.click();
+        await PurchasePage.btnCheckout.waitForClickable();
+        await PurchasePage.btnCheckout.click();
+        await PurchasePage.purchase('matias','raviola','12345');
+        await PurchasePage.btnFinish.click();
+        await PurchasePage.btnBackHome.waitForClickable();
+        await PurchasePage.btnBackHome.click();
+        await PurchasePage.hamburMenu.click();
+        await PurchasePage.logout.waitForClickable();
+        await PurchasePage.logout.click();
+    });
+    it('complete the sell without a product in the cart', async () => {
+        await PurchasePage.login('standard_user','secret_sauce');
+        await expect(browser).toHaveUrl("https://www.saucedemo.com/inventory.html");
+        await PurchasePage.btnCart.waitForClickable();
+        await PurchasePage.btnCart.click();
+        await PurchasePage.btnCheckout.waitForClickable();
         await PurchasePage.btnCheckout.click();
         await PurchasePage.purchase('matias','raviola','12345');
         await PurchasePage.hamburMenu.click();
+        await PurchasePage.logout.waitForClickable();
+        await PurchasePage.logout.click();
+    });
+    it('add all the products to the cart', async () => {
+        await PurchasePage.login('standard_user','secret_sauce');
+        await expect(browser).toHaveUrl("https://www.saucedemo.com/inventory.html");
+        await PurchasePage.btnAddBackpack.waitForClickable();
+        await PurchasePage.btnAddBackpack.click();
+        await PurchasePage.btnAddBikeLight.click();
+        await PurchasePage.btnAddBoltTshirt.click();
+        await PurchasePage.btnAddFleeceJacket.click();
+        await PurchasePage.btnAddLabsOnesie.click();
+        await PurchasePage.btnAddTshirtRed.click();
+        const cartNumber = await $('#shopping_cart_container > a > span').getText();
+        await expect(cartNumber).toBe('6')
+    });
+        it('descart all the products to the cart', async () => {
+        await PurchasePage.hamburMenu.click();
+        await PurchasePage.resetCart.waitForClickable();
+        await PurchasePage.resetCart.click();
+        const cart = await $('#shopping_cart_container > a')
+        await expect(cart).toHaveChildren(0)
+        await PurchasePage.btnRemoveBackpack.click();
+        await PurchasePage.btnRemoveBikeLight.click();
+        await PurchasePage.btnRemoveBoltTshiert.click();
+        await PurchasePage.btnRemoveFleeceJacket.click();
+        await PurchasePage.btnRemoveLabsOnesie.click();
+        await PurchasePage.btnRemoveTshirtRed.click();
+        await PurchasePage.hamburMenu.click();
+        await PurchasePage.logout.waitForClickable();
+        await PurchasePage.logout.click();
+    });
+    it('complete the sell with empty/imcomplete checkout information', async () => {
+        await PurchasePage.login('standard_user','secret_sauce');
+        await expect(browser).toHaveUrl("https://www.saucedemo.com/inventory.html");
+        await PurchasePage.btnAddBackpack.waitForClickable();
+        await PurchasePage.btnAddBackpack.click();
+        const cartNumber = await $('#shopping_cart_container > a > span').getText();
+        await expect(cartNumber).toBe('1');
+        await PurchasePage.btnCart.waitForClickable();
+        await PurchasePage.btnCart.click();
+        await PurchasePage.btnCheckout.waitForClickable();
+        await PurchasePage.btnCheckout.click();
+        await PurchasePage.purchase('','','');
+        await expect(PurchasePage.errorMsg).toHaveText("Error: First Name is required");
+        await PurchasePage.purchase('','raviola','12345');
+        await expect(PurchasePage.errorMsg).toHaveText("Error: First Name is required");
+        await PurchasePage.purchase('matias','','12345');
+        await expect(PurchasePage.errorMsg).toHaveText("Error: Last Name is required");
+        await PurchasePage.purchase('matias','raviola','');
+        await expect(PurchasePage.errorMsg).toHaveText("Error: Postal Code is required");
+        await PurchasePage.hamburMenu.click();
+        await PurchasePage.logout.waitForClickable();
         await PurchasePage.logout.click();
     });
 });
